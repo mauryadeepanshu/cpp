d2e8c84d6065541b3f6dd2d0549e72ef1ccbdaae
Deepanshu <mauryadeepanshu240@gmail.com>
2023-12-20 16:40:29 +0530

    sum_of_n_integers

---
+++ sum of first n numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+ int main  () {
+   int x;
+   int i = 1;
+   int sum = 0;
+   cout << "Enter the number to find sum of first n numbers: ";
+   cin >> x;
+   
+   while (i <= x) {
+   sum += i;
+   i++;
+   }
+   
+   cout << "Sum of the first " <<  x  << " number is " << sum;
+   
+    
+
+
+return 0;
+  }
+ 
