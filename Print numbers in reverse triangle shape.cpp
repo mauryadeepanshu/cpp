3f698ba352713361c943590b3a601571e812ced7
Deepanshu <mauryadeepanshu240@gmail.com>
2023-12-25 16:01:16 +0530

    Triangle shape

---
+++ Print numbers in reverse triangle shape.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+  int main () {
+  int x,i,j;
+  
+  cout<< "Print trianglr in reverse order: ";
+  cin >>x;
+  
+  for (i = 1; i <= x; i++) {
+  for (j = 1; j <= x-i+1; j++) {
+     cout << x;
+  }
+   cout << endl;
+  }
+return 0;
+  }  
+ 
